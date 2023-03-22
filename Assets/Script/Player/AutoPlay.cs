768bda41da29c2513242bfd3dd4e5eb2bc9ffa55
kv0630028 <66360775+kv0630028@users.noreply.github.com>
2023-03-22 16:34:42 +0900

    Auto 스크립트

---
+++ Assets/Script/Player/AutoPlay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPlay : MonoBehaviour
+{    
+    void Update()
+    {
+        
+    }
+
+    void Check()
+    {
+
+    }
+}
