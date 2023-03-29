c9ed2d700ba733801979da2d4b2cf7784528a2d7
HY <kv0630028@gmail.com>
2023-03-30 01:18:55 +0900

    상점

--- Assets/Script/LobbySceneEvent.cs
+++ Assets/Script/LobbySceneEvent.cs
@@ -6,8 +6,19 @@ using UnityEngine.Events;
 
 public class LobbySceneEvent : SceneChangeEvnet
 {
+    public GameObject store;
+
+    public void UICheck()
+    {
+        if (store.activeSelf == false)
+            store.SetActive(true);
+        else
+            store.SetActive(false);
+    }
+
+
     public override void Fade_ButtonTouch()
     {
         base.Fade_ButtonTouch(() => sceneChange.Change(SceneChange.eSCENE_TYPE.INGAME));
-    }
+    }    
 }
