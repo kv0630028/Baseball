103b379d3e07b7a20d73252a09a430e91192586a
kv0630028 <66360775+kv0630028@users.noreply.github.com>
2023-04-06 14:45:04 +0900

    BGM

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Bgm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bgm.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
