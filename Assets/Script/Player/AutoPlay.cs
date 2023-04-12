bd929392d94e312b2c931e6f07ccd2a5d8dc9d4b
kv0630028 <66360775+kv0630028@users.noreply.github.com>
2023-04-12 12:00:17 +0900

    자동기능
    색변경

--- Assets/Script/Player/AutoPlay.cs
+++ Assets/Script/Player/AutoPlay.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AutoPlay : MonoBehaviour
 {
+    public Button btn;
     public bool isAuto = false;
     
-    void Update()
+    void Start()
     {
         
     }    
@@ -23,7 +25,23 @@ public class AutoPlay : MonoBehaviour
 
     public void AutoBtn()
     {
+        ColorBlock colorBlock = btn.colors;
         isAuto = !isAuto;
+        if (isAuto == true)
+        {            
+            colorBlock.normalColor = new Color(0f, 1f, 0f, 1f);
+            colorBlock.highlightedColor = new Color(0f, 1f, 0f, 1f);
+            colorBlock.selectedColor = new Color(0f, 1f, 0f, 1f);
+            btn.colors = colorBlock;
+        }
+        else
+        {
+            colorBlock.normalColor = new Color(1f, 1f, 1f, 1f);
+            colorBlock.highlightedColor = new Color(1f, 1f, 1f, 1f);
+            colorBlock.selectedColor = new Color(1f, 1f, 1f, 1f);
+            btn.colors = colorBlock;
+        }
+
     }
 
 }
